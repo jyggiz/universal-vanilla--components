1a15041b4389ec948a920891715f28d87df2ce03
Timur Zhigmytov <TZhigmytov@dar.io>
2023-12-22 17:59:35 +0600

    Add skeleton for tabbed interface component

---
+++ components/tabbed-interface/index.js
@@ -0,0 +1,7 @@
+// Choose the default section on page load
+// And scroll to the top to show nav
+window.onload = function () {
+    window.location.hash = 'section1'
+    document.documentElement.scrollTop = 0
+  }
+  
